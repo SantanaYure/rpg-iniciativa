42dd033f765d6ce5b5d9cedcd0eb947c08b89728
SantanaYure <yure.s.santana@outlook.com>
2025-05-06 11:18:00 -0300

    feat(functions): Adiciona função createUserProfile e ajusta dependências

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -1,19 +1,24 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+admin.initializeApp();
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
-
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.createUserProfile = functions.auth.user.onCreate(async (user) => {
+  const userProfileRef = admin.firestore()
+      .collection("users")
+      .doc(user.uid);
+  try {
+    await userProfileRef.set({
+      email: user.email,
+      displayName: user.displayName || "",
+      createdAt: admin.firestore.FieldValue.serverTimestamp(),
+      lgpdConsent: {
+        termsAccepted: false,
+        consentTimestamp: null,
+      },
+    });
+    console.log(`Perfil criado com sucesso para: ${user.uid}`);
+  } catch (error) {
+    console.error(`Erro ao criar perfil para ${user.uid}:`, error);
+  }
+});
